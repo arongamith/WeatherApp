c4e638f6a1a085a7d8fd54ac35fd8d8e2ba4ce08
arongamith <arongamith@gmail.com>
2024-05-26 17:47:01 +0530

    adding the whole app

---
+++ src/constants/index.js
@@ -0,0 +1,21 @@
+export const apiKey = '9eee42c80c704687af0130609242405'
+
+export const weatherImages = {
+    'Partly cloudy': require('../assets/partlyCloudy.png'),
+    'Moderate rain': require('../assets/moderateRain.png'),
+    'Patchy rain nearby': require('../assets/rain.png'),
+    'Sunny': require('../assets/sunsun.png'),
+    'Clear': require('../assets/sunny.png'),
+    'Overcast': require('../assets/cloudy.png'),
+    'Cloudy': require('../assets/clouds.png'),
+    'Light rain': require('../assets/raindrop.png'),
+    'Moderate rain at times': require('../assets/moderateRain.png'),
+    'Heavy rain': require('../assets/heavyrain.png'),
+    'Heavy rain at times': require('../assets/heavyrain.png'),
+    'Moderate or heavy freezing rain': require('../assets/heavyrain.png'),
+    'Moderate or heavy rain shower': require('../assets/heavyrain.png'),
+    'Moderate or heavy rain with thunder': require('../assets/heavyrain.png'),
+    'other': require('../assets/moderateRain.png'),
+    'Mist': require('../assets/mist.png'),
+    'Patchy light rain in area with thunder': require('../assets/rain.png')
+}
